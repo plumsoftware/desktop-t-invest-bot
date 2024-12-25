89be510067c5e2433a485f041030cdc2ccefc160
Slava <plumsoftwareofficial@gmail.com>
2024-12-25 10:55:42 +0600

    composeApp [+] Добавил тему

---
+++ composeApp/src/main/kotlin/ru/plumsoftware/ui/theme/Shapes.kt
@@ -0,0 +1,11 @@
+package ru.plumsoftware.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(18.dp),
+    large = RoundedCornerShape(28.dp)
+)
