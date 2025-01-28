56e54a3277f20fc88f14c4d512d56779f07cfe27
Slava <plumsoftwareofficial@gmail.com>
2025-01-28 10:41:17 +0600

    Client [+] Добавил репозиторий для статистики.

---
+++ net-core/src/main/kotlin/ru/plumsoftware/net/core/routing/StatisticRouting.kt
@@ -0,0 +1,9 @@
+package ru.plumsoftware.net.core.routing
+
+object StatisticRouting {
+    const val GET_STATISTIC = "statistic"
+
+    object Params {
+        const val ID = "id"
+    }
+}
--- trading-bot-server/src/main/kotlin/ru/plumsoftware/plugins/statistic/ConfigureStatistic.kt
+++ trading-bot-server/src/main/kotlin/ru/plumsoftware/plugins/statistic/ConfigureStatistic.kt
@@ -9,6 +9,7 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import ru.plumsoftware.facade.StatisticFacade
 import ru.plumsoftware.net.core.model.receive.StatisticReceive
+import ru.plumsoftware.net.core.routing.StatisticRouting
 import ru.plumsoftware.plugins.auth.AuthConfig
 import ru.plumsoftware.service.statistic.StatisticService
 import javax.management.InvalidApplicationException
@@ -21,9 +22,9 @@ fun Application.configureStatistic() {
 
     routing {
         authenticate(AuthConfig.BEARER_AUTH) {
-            get(path = "statistic") {
+            get(path = StatisticRouting.GET_STATISTIC) {
                 try {
-                    val id = call.request.queryParameters["id"]
+                    val id = call.request.queryParameters[StatisticRouting.Params.ID]
                         ?: throw InvalidApplicationException("Invalid id")
 
                     val list: List<StatisticReceive> = statisticFacade.get(id = id.toLong())
